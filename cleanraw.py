c1b51fb5a62c8bbf0caf3a54cb6cd5ef66427739
sharkberto <alex.aruj@gmail.com>
2015-06-30 10:16:21 -0700

    palo extractor code introduced, clean and unclean samples separated

--- cleanraw.py
+++ cleanraw.py
@@ -1,12 +1,16 @@
 import re
 from organizeraw import chunks
+
 # match char-only string, no nums between a period and space, and tag '</p>'
 artist_regex = re.compile('(?<=\W\s)(\D*?)(?=\<\/[p])') 
 artist_regex_in_track = re.compile('(?<=\D[\)\.]\s)(\D*?)(?=<BR>)') # must have closing parentheses or period for backref
+unclean_training_samples = []
+clean_training_samples = []
 
 #non-digits b/w non-alphanum and non-alphanum
 song_title_regex = re.compile('([A-Z].*?)(?=<BR>)')
 
+# ***** ARTIST EXTRACTION **** goes into position 0
 for i in range(len(chunks)):
     artistSearchInHeader = re.search(artist_regex, chunks[i][0]) 
     if artistSearchInHeader == None:
@@ -45,7 +49,8 @@ for i in range(len(chunks)):
                 chunks[i][j] = [artistSearchInTrack.group(0), chunks[i][j]]
          
 
-## extract song title from position one, place at position one, lyrics get bumped to pos 2
+## **** SONG TITLE **** song goes into position 1, letras at position 2
+                
 for i in range(len(chunks)):
     for j in range(len(chunks[i])):
         songTitleSearch = re.search(song_title_regex, chunks[i][j][1])
@@ -54,16 +59,36 @@ for i in range(len(chunks)):
         else:
             chunks[i][j].insert(1, 'NA')
 
+## ***** PALO **** goes into position 2, letras go to position 3
+
+palo_regex = re.compile('(?<=\()(.*?)(?=\))')
+
+for i in range(len(chunks)):
+    for j in range(len(chunks[i])):
+        extracted_palo = re.search(palo_regex, chunks[i][j][2])
+        if extracted_palo == None:      
+            chunks[i][j].insert(2, 'NA')
+            unclean_training_samples.append(chunks[i][j])       
+        else:    
+            chunks[i][j].insert(2, extracted_palo.group(0))
+        
+## **** VERSES **** verse count goes to position 3, letra goes into position 4
 
 # Count number of verses in each letra, append to end of chunk[i][j], delete tags
-##for i in range(len(chunks)):
-##    for j in range(len(chunks[i])):
-##        chunks[i][j].insert(3,chunks[i][j][2].count('<P>'))
 
-##        extract title before these cleaning steps
-##        chunks[i][j] = re.sub('<.*?>', '', chunks[i][j])
-##        chunks[i][j] = re.sub('"<p."', '', chunks[i][j])
-##        chunks[i][j] = re.sub('<p style="font:bold', '', chunks[i][j])
-##        chunks[i][j] = re.sub('\n', ' ', chunks[i][j])
+for i in range(len(chunks)):
+    for j in range(len(chunks[i])):
+        chunks[i][j].insert(3,chunks[i][j][2].count('<P>'))
+
+## ***** CLEAN OUT HTML *****
+        
+for i in range(len(chunks)):
+    for j in range(len(chunks[i])):
+        chunks[i][j] = re.sub('<.*?>', '', chunks[i][j][4])
+        chunks[i][j] = re.sub('"<p."', '', chunks[i][j][4])
+        chunks[i][j] = re.sub('<p style="font:bold', '', chunks[i][j][4])
+        chunks[i][j] = re.sub('\n', ' ', chunks[i][j][4])
+
+## SOME ISSUES ARE EXPECTED WITH THE EXTRACTION. DATA WILL HAVE TO BE CLEANED LATER
 
     
