29f9b24977b0ceafbc52a85663a6426825ba6a10
sharkberto <alex.aruj@gmail.com>
2015-07-03 20:12:14 -0700

    updated song-title regex. the file true_palos has manually updated flamenco style names and should not be written over by program from here one

--- cleanraw.py
+++ cleanraw.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 import re
 from organizeraw import chunks
 
 # match char-only string, no nums between a period and space, and tag '</p>'
-artist_regex = re.compile('(?<=\W\s)(\D*?)(?=\<\/[p])') 
-artist_regex_in_track = re.compile('(?<=\D[\)\.]\s)(\D*?)(?=<BR>)') # must have closing parentheses or period for backref
+artist_regex = re.compile('(?<=\W\s)(\D*?)(?=\<\/p>)') 
+artist_regex_in_track = re.compile('(?<=\D[\)\.]\s)(\D*?)(?=\/p>)')
 #non-digits b/w non-alphanum and non-alphanum
-song_title_regex = re.compile('([A-Z].*?)(?=<BR>)')
+song_title_regex = re.compile('([A-Z].*?)()(?=[\.\(\<])')#old= ('([A-Z].*?)(?=\W\s)
 palo_regex = re.compile('(?<=\()(.*?)(?=\))')
 
 lyrics = []
@@ -84,7 +85,7 @@ for i in range(len(chunks)):
 ## ***** LYRICS ***** for each chunks except the header...
 
 
-## ***** CLEAN OUT HTML *****
+## ***** CLEAN OUT HTML FROM LYRICS *****
 
 song_header_regex = re.compile('^.*?<\/p>')
 for i in range(len(chunks)):
@@ -98,12 +99,33 @@ for i in range(len(chunks)):
         
         lyrics.append(chunks[i][j])
         
-
-
-
-
         
-## lyrics are everything after the closing </p> tag
-## SOME ISSUES ARE EXPECTED WITH THE EXTRACTION. DATA WILL HAVE TO BE CLEANED LATER
+## ***** CLEAN OUT HTML FROM ARTISTS AND SEND FEATURES TO FILES*****
+with open('lyrics.txt', 'w+') as lyrics_doc:
+    with open('palos.txt', 'w+') as palos_doc:
+        with open('verse_counts.txt', 'w+') as verse_counts_doc:
+            with open('testlyrics.txt', 'w+') as testlyrics:
+                with open('testpalos.txt', 'w+') as testpalos:
+                    with open('testverse_counts.txt', 'w+') as testverse_counts:
+                        with open('song_titles.txt', 'w+') as songs:
+                            for i in range(len(lyrics)):
+                                lyrics[i] = re.sub('\r\n', '', lyrics[i])
+                                songs.write(song_titles[i].encode('utf-8') + '\n')
+                                #if the palo is valid, we can use the lyrics in training
+    ##                            if palos[i][0].islower():
+                                lyrics_doc.write(lyrics[i].encode('utf-8') + '\n')
+                                palos_doc.write(palos[i].encode('utf-8') + '\n')
+                                verse_counts_doc.write(str(verse_counts[i]) + '\n')
+    ##                            else:
+    ##                                testlyrics.write(lyrics[i].encode('utf-8') + '\n')
+    ##                                testpalos.write(palos[i].encode('utf-8') + '\n')
+    ##                                testverse_counts.write(str(verse_counts[i]) + '\n')
+##
+##
+## SOME ISSUES ARE EXPECTED WITH THE EXTRACTION. DATA WILL HAVE TO BE CLEANED AND
+## DATA ZIPPED TOGETHER IN COMPREHENSIVE FORMAT                           
 
+## to create dictionary of items for json dict to pass to Dictvectorizer in scikitleanr
+## palo_song_dict = dict(zip(palos,song_titles)
+## 
     
