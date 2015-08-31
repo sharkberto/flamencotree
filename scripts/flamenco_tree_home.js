2cc588ffb2e92cc027c3ad65ece0b7f79ae1544e
sharkberto <alex.aruj@gmail.com>
2015-08-30 17:15:23 -0700

    first commit to jquerytest

--- scripts/flamenco_tree_home.js
+++ scripts/flamenco_tree_home.js
@@ -1,20 +1,38 @@
-
-
 $(document).ready(function() {
 
+	var curpalo = "placeholder";
 	
-	$(".song").hide();
+	//JSON array of palos
+	var nodes = jQuery.parseJSON('{"name":"Tonás"}');
+  /*{"name":"Martinetes","group":1},
+  {"name":"Debla","group":1},
+  {"name":"Pregones","group":1}'
+  );*/
+	
+	//TODO:
+	// use .append to traverse through tree
+	// Once palo children are done, show the songs.
+	$().each(nodes, function(i, item) {
+		alert(name);
+		//create a div for each palo in the json read in
+	});	
+	// When palo clicked, append divs
     $(".palo").click(function() {
-        //$(".palo").fadeOut('slow',0);
-		$(".song").toggle();
+	
+        $(".palo").text(function(){
+			return curpalo;
+		});
+		
+		//$(".song").toggle();
     });
 	
-	 $(".song").click(function() {
-        $(".song").hide();
+	$(".song").click(function() {
+       $(".song").hide();
     }); 
+
 });
 
-//a
+
 
 
 
