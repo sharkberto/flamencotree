735d5efd9c585eb5b880a47c9b695b29cde42034
sharkberto <alex.aruj@gmail.com>
2015-09-20 16:55:16 -0700

    embedded iframe into div. next step is to create div dynamically and load video from JS file rather than html

--- scripts/flamenco_tree_home.js
+++ scripts/flamenco_tree_home.js
@@ -3,7 +3,7 @@ $(document).ready( function() {
 	var newdiv = document.createElement( "div" );
 	var nodes = jQuery.parseJSON('[{"name":"Tonás"},{"name":"Martinetes"},{"name":"Debla"},{"name":"Pregones"}]');//'[{"name":"Tonás"},{"name":"Martinetes"},{"name":"Debla"},{"name":"Pregones"}]');
 	
-	$('.explorer').hide();
+	$('#player').hide();
 	
     $('.palo').hover( 
 	  	function(){
@@ -12,6 +12,7 @@ $(document).ready( function() {
 				$.each(nodes, function(i, item) {	
 				$(".palo").append(item.name); // adjust to create new divs
 				*/
+				
 		},
 		function() {
 			$(this).removeClass('active');
@@ -19,7 +20,49 @@ $(document).ready( function() {
 	  );
 	  
 	$('.palo').click( function(){
-		$('.explorer').fadeToggle();
+			
+		$("#player").fadeToggle();
+		      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+
 			
      }); 
 	
